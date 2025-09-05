166615fa76c9acfde6d1c0814a4460f8fdfe7746
ValterEmilio <valternic77@gmail.com>
2025-09-04 23:05:59 -0300

    Create Aula12.js

---
+++ aula21.js
@@ -0,0 +1,11 @@
+function matematica(){
+    let num1 = 5
+    let num2 = 7
+    let res = num1 * num2
+    if(res%2 == 0)
+        return "par"
+    else
+        return "impar"
+}
+
+console.log(matematica())
