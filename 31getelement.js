fd1879078c27240473cc8a6ed21e27c9fce24d34
ValterEmilio <valternic77@gmail.com>
2025-09-11 09:43:10 -0300

    aula 31 getelementbyid
    defe

--- 31getelement.js
+++ 31getelement.js
@@ -1,3 +1,19 @@
-const curso = document.getElementById('div2')
+const curso1 = document.getElementById('div1')
+const curso2 = document.getElementById('div2')
+const curso3 = document.getElementById('div3')
+const curso4 = document.getElementById('div4')
+const curso5 = document.getElementById('div5')
+const curso6 = document.getElementById('div6')
 
-curso.innerHTML = "Valter"
+// console.log(curso)
+// console.log(curso.id)
+// console.log(curso.innerHTML)
+// curso.innerHTML = "Valter"
+
+const elementos = [ curso1, curso2, curso3, curso4, curso5, curso6]
+
+elementos.map((e)=> {
+    e.style.fontFamily = 'Arial'
+    //console.log(e)
+})
+//show de bolice!!!
