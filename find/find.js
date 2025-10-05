90421f5b66208a1e8775857010fec34e76ccad06
ValterEmilio <valternic77@gmail.com>
2025-10-05 16:45:42 -0300

    create find.js

---
+++ find/find.js
@@ -0,0 +1,4 @@
+const p_array=document.querySelector("#array")
+const txt_pesquisar=document.querySelector('#txt_pesquisar')
+const btnPesquisar=document.querySelector('#btnPesquisar')
+const resultado=document.querySelector("#resultado")
